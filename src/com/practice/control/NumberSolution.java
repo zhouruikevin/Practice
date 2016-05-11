1e647ac346d74a0c5899c148f882074b268e253b
zhourui_kevin <zhourui_kevin@163.com>
2016-05-12 01:07:07 +0800

    NumberSolution
    NumberSolution

--- src/com/practice/control/NumberSolution.java
+++ src/com/practice/control/NumberSolution.java
@@ -1,5 +1,9 @@
 package com.practice.control;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class NumberSolution {
 	public static int NumberOf1(int n) {
 		String binaNStr = Integer.toBinaryString(n).replaceAll("0", "");
@@ -8,5 +12,61 @@ public class NumberSolution {
 
 	public double Power(double base, int exponent) {
 		return Math.pow(base, exponent);
+	}
+
+	public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if (input == null || input.length < k || k <= 0) {
+			return list;
+		}
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < input.length - i - 1; j++) {
+				if (input[j] < input[j + 1]) {
+					int temp = input[j];
+					input[j] = input[j + 1];
+					input[j + 1] = temp;
+				}
+			}
+			list.add(input[input.length - i - 1]);
+		}
+		return list;
+	}
+
+	public String PrintMinNumber(int[] numbers) {
+		if (numbers == null || numbers.length == 0) {
+			return "";
+		}
+		int length = numbers.length;
+		String[] numStrs = new String[length];
+		for (int i = 0; i < length; i++) {
+			numStrs[i] = numbers[i] + "";
+		}
+		Arrays.sort(numStrs, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				String total1 = s1 + s2;
+				String total2 = s2 + s1;
+				return total1.compareTo(total2);
+			}
+		});
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			sb.append(numStrs[i]);
+		}
+		return sb.toString();
+	}
+
+	public int NumberOf1Between1AndN_Solution(int n) {
+		int count = 0;
+		for (int i = 0; i <= n; i++) {
+			String str = i + "";
+			char[] chars = str.toCharArray();
+			for (int j = 0; j < chars.length; j++) {
+				if (chars[j] == '1') {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 }
--- src/com/practice/test/junit3/TestNumber.java
+++ src/com/practice/test/junit3/TestNumber.java
@@ -1,14 +1,52 @@
 package com.practice.test.junit3;
 
+import java.util.ArrayList;
+
 import com.practice.control.NumberSolution;
 
 import junit.framework.TestCase;
 
 public class TestNumber extends TestCase {
-	public void testNumberOf1() {
-		assertEquals(1, NumberSolution.NumberOf1(1));
-		assertEquals(2, NumberSolution.NumberOf1(3));
-		assertEquals(2, NumberSolution.NumberOf1(5));
-		assertEquals(1, NumberSolution.NumberOf1(8));
+
+	// public void testNumberOf1() {
+	// assertEquals(1, NumberSolution.NumberOf1(1));
+	// assertEquals(2, NumberSolution.NumberOf1(3));
+	// assertEquals(2, NumberSolution.NumberOf1(5));
+	// assertEquals(1, NumberSolution.NumberOf1(8));
+	// }
+
+	private NumberSolution solution;
+	// private int[] array = { 1, 6, 5, 6, 1, 2, 3, 4 };
+	private ArrayList<Integer> aMin2;
+	private ArrayList<Integer> aMin3;
+	private ArrayList<Integer> aMin4;
+	// aMin2 = new ArrayList<Integer>();
+	// aMin2.add(1);
+	// aMin2.add(1);
+	// aMin3 = new ArrayList<Integer>(aMin2);
+	// aMin3.add(2);
+	// aMin4 = new ArrayList<Integer>(aMin3);
+	// aMin4.add(3);
+	private int[] array = { 3, 32, 321 };
+	private String result = "321323";
+
+	@Override
+	protected void setUp() throws Exception {
+		solution = new NumberSolution();
+		super.setUp();
+
 	}
+
+//	public void testPrintMinNumber() {
+//		assertEquals(result, solution.PrintMinNumber(array));
+//	}
+
+	public void testNumberOf1Between1AndN_Solution() {
+		assertEquals(6, solution.NumberOf1Between1AndN_Solution(13));
+	}
+	// public void testGetLeastNumbers_Solution() {
+	// assertEquals(aMin2, solution.GetLeastNumbers_Solution(array, 2));
+	// assertEquals(aMin3, solution.GetLeastNumbers_Solution(array, 3));
+	// assertEquals(aMin4, solution.GetLeastNumbers_Solution(array, 4));
+	// }
 }
